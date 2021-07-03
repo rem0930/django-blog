0c4b93056e3b4ffe1984e7e479483bd3da512869
rem0930 <06araki@gmail.com>
2021-07-03 10:47:06 +0900

    完成

---
+++ app/views.py
@@ -0,0 +1,40 @@
+from django.views.generic import View
+from django.shortcuts import render
+from .models import Post
+from .forms import PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class CreatePostView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        return render(request, 'app/post_form.html', {
+            'form': form
+        })
+    
+    def post(self, request, *args, **kwargs):
+        form = PostForm(request.POST or None)
+
+        if form.is_valid():
+            post_data = Post()
+            post_data.author = request.user
+            post_data.title = form.cleaned_data['title']
+            post_data.content = form.cleaned_data['content']
+            post_data.save()
+            return redirect('post_detail', post_data.id)
+
+        return render(request, 'app/post_form.html', {
+            'form': form
+        })
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        return render(request, 'app/post_delete.html', {
+            'post_data': post_data
+        })
+
+    def post(self, request, *args, **kwargs):
+        post_data = Post.objects.get(id=self.kwargs['pk'])
+        post_data.delete()
+        return redirect('index')
